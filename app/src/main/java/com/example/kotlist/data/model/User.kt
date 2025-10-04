2fe3e5d2aaccb47ee0b8cc1d5edbfa44b436369d
Ícaro Pavan <icaro.pavan97@gmail.com>
2025-10-04 13:53:13 -0300

    feat: added list adding logic and add list activity improvements

--- app/src/main/java/com/example/kotlist/data/model/User.kt
+++ app/src/main/java/com/example/kotlist/data/model/User.kt
@@ -1,6 +1,9 @@
 package com.example.kotlist.data.model
 
+import java.util.UUID
+
 data class User (
+    val id: String = UUID.randomUUID().toString(),
     val name: String,
     val email: String,
     val password: String
--- app/src/main/java/com/example/kotlist/layoutlogic/AddListActivity.kt
+++ app/src/main/java/com/example/kotlist/layoutlogic/AddListActivity.kt
@@ -1,17 +1,37 @@
 package com.example.kotlist.layoutlogic
 
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlist.R
+import com.example.kotlist.data.model.ShoppingList
+import com.example.kotlist.data.repository.ShoppingListRepository
+import com.example.kotlist.data.repository.UserRepository
 import com.example.kotlist.databinding.ActivityAddListBinding
 import com.example.kotlist.databinding.ActivityLoginBinding
+import androidx.core.net.toUri
 
 class AddListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddListBinding
+    private var listCoverImageSelectedUri: String? = null
+    private var placeholderImageId: Int = -1
+
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            listCoverImageSelectedUri = uri.toString()
+            binding.addListImagePreview.setImageURI(uri)
+        } else {
+            Toast.makeText(this, "Nenhuma imagem selecionada.", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +53,46 @@ class AddListActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        placeholderImageId = randomizeListCoverPlaceholder()
+
+        binding.addListAddImage.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+        binding.addListAddListButton.setOnClickListener {
+            val listTitle: String = binding.addListListNameInput.text.toString().trim()
+            val coverImageUri: String? = listCoverImageSelectedUri
+            val placeholderImageId: Int = placeholderImageId
+            val userId: String? = UserRepository.getUserLoggedIn()?.id
+
+            createNewList(listTitle, coverImageUri, placeholderImageId, userId)
+        }
+
+        // adicionar listener do botao de voltar para a tela principal
+    }
+
+    fun randomizeListCoverPlaceholder(): Int {
+        val randomizedPlaceholder = ShoppingListRepository.getRandomPlaceholderId()
+        binding.addListImagePreview.setImageURI("android.resource://$packageName/$randomizedPlaceholder".toUri())
+        return ShoppingListRepository.getRandomPlaceholderId()
+    }
+
+    fun createNewList(listTitle: String, coverImageUri: String?, placeholderImageId: Int, userId: String?) {
+        if(listTitle.isEmpty()) {
+            binding.addListListNameInputWrapper.error = "A lista deve ter um nome."
+            return
+        }
+
+        if(userId == null) {
+            Toast.makeText(this, "Houve um erro ao tentar criar a lista.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val newList = ShoppingList(title = listTitle, coverImageUri = coverImageUri, placeholderImageId = placeholderImageId, userId = userId)
+        ShoppingListRepository.addList(newList)
+
+        Toast.makeText(this, "Nova lista criada com sucesso!", Toast.LENGTH_SHORT).show()
+        finish()
     }
 }
