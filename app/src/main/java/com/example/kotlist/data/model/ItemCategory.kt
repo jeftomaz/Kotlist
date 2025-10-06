c0eaa41c76c755580b0447eadbe76b8454923d97
Ícaro Pavan <icaro.pavan97@gmail.com>
2025-10-06 16:26:17 -0300

    feat: added new category icons

--- app/src/main/java/com/example/kotlist/data/model/ItemCategory.kt
+++ app/src/main/java/com/example/kotlist/data/model/ItemCategory.kt
@@ -8,10 +8,10 @@ enum class ItemCategory (
     @StringRes val categoryNameId: Int,
     @DrawableRes val categoryIconId: Int
 ) {
-    FRUTAS(R.string.food_category, R.drawable.meat_icon),
-    BEBIDAS(R.string.drink_category, R.drawable.meat_icon),
-    ELETRONICOS(R.string.electronics_category, R.drawable.meat_icon),
-    HIGIENE(R.string.hygiene_category, R.drawable.meat_icon),
-    UTENSILIOS(R.string.utensils_category, R.drawable.meat_icon),
-    OUTROS(R.string.others_category, R.drawable.meat_icon);
+    COMIDA(R.string.food_category, R.drawable.meat_icon),
+    BEBIDAS(R.string.drink_category, R.drawable.drinks_icon),
+    ELETRONICOS(R.string.electronics_category, R.drawable.electronics_icon),
+    HIGIENE(R.string.hygiene_category, R.drawable.hygiene_icon),
+    UTENSILIOS(R.string.utensils_category, R.drawable.utensils_icon),
+    OUTROS(R.string.others_category, R.drawable.others_icon);
 }
