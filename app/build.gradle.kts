f7591e171eead5562d6b35a6bc982dd7fb93bc55
Jeferson Tomaz <jeferson.tomaz.contact@gmail.com>
2025-11-12 01:22:25 -0300

    Feature: SignUpViewModel included

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,12 @@ android {
 
 dependencies {
     implementation(libs.jbcrypt)
-    implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil:2.7.0")
 
     // Dependências para ViewModel e LiveData
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.activity:activity-ktx:1.11.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
